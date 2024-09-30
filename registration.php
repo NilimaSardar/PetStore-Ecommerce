30469b338c8c34e098efad9297e0987e5ed45a02
NilimaSardar <snilima181@gmail.com>
2024-09-30 16:28:59 +0545

    add all the file basic ui completed

---
+++ registration.php
@@ -0,0 +1,60 @@
+<!-- <section class="show-reg"> -->
+    <div class="container">
+        <div class="row">  
+            <h3 class="float-right">
+                <button type="button" class="close-reg">
+                    <span>&times;</span>
+                </button>
+            </h3>
+
+            <div class="form-content">
+                <h3 class="text-center">Create New Account</h3>
+                <hr>
+
+                <form action="" id="registration">
+                        
+                        <div class="col">
+                            
+                            <div class="form-group">
+                                <label for="" class="control-label">Firstname</label>
+                                <input type="text" class="form-control" name="firstname" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="" class="control-label">Lastname</label>
+                                <input type="text" class="form-control" name="lastname" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="" class="control-label">Contact</label>
+                                <input type="number" class="form-control" name="contact" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="" class="control-label">Gender</label>
+                                <select name="gender" id="" class="custom-select select" required>
+                                    <option>Male</option>
+                                    <option>Female</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <label for="" class="control-label">Default Delivery Address</label>
+                                <input type="text" class="form-control" name="default_delivery_address" required>
+                                </div>
+                                <div class="form-group">
+                                <label for="" class="control-label">Email</label>
+                                <input type="text" class="form-control" name="email" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="" class="control-label">Password</label>
+                                <input type="password" class="form-control" name="password" required>
+                            </div>
+                            <div class="form-group-submit">
+                                <a href="#" id="login-show">Already have an Account</a>
+                                <button class="btn">Register</button>
+                            </div>
+                        </div>
+                </form>
+            </div>
+        </div>
+    </div>
+<!-- </section> -->
